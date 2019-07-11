c764e6ff70fc7bc0311245c10d57eb413b439018
Anthony Graniero <ananfro@gmail.com>
2019-07-11 18:08:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,9 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile_page = Nokogiri::HTML(open(profile_url))
+    student = {}
+    links = profile_page.css(".social-icon-container").children.css("a").map 
   end
 
 end
